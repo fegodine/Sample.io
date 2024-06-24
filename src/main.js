edd9a009977ae986399457c1a581db82689d7ef8
fegodine <fegodine@cisco.com>
2024-06-24 11:34:06 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ function setup() {
   Alpine.store('model', {
     currentPage: 'home', // 'home', 'service'
     currentLanguage: 'english',
-    dialNumber: 'erica.talking@ivr.vc',
+    dialNumber: 'dps-desk_pro@cwaengineering-sbx.rooms.webex.com',
     services: [],
 
     init() {
